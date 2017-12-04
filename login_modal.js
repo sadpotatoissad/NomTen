0e12ffb39af091463dcf6258d92c049e97d8d708
David <davidw2009@hotmail.com>
2017-12-04 05:50:32 -0500

    reloading items back to fridge when login in works

--- login_modal.js
+++ login_modal.js
@@ -1,5 +1,5 @@
     var userID;
-    var ngrokURL = "http://aa1fd8c8.ngrok.io";
+    var ngrokURL = "http://30eed78b.ngrok.io";
     document.ready
     $(document).ready(function(){
       if(document.cookie != ''){
@@ -49,7 +49,30 @@
 
                         $.get(ngrokURL + '/users/' + userID, function(user_data){
                             console.log("the user data is: " + user_data);
-                            console.log("the status is ");
+                            
+                            var u_id = user_data[0]. user_id;
+                            var u_proteinList = user_data[0].proteinList;
+                            var u_carbList = user_data[0].carbList;
+                            var u_dairyList = user_data[0].dairyList;
+                            var u_vegList = user_data[0].vegList;
+                            var u_fruitList = user_data[0].fruitList;
+                            var u_miscList = user_data[0].miscList;
+
+                            reloadFridge('proteinList', u_proteinList);
+
+                            reloadFridge('carbList', u_carbList);
+
+                            reloadFridge('dairyList', u_dairyList);
+
+                            reloadFridge('vegList', u_vegList);
+
+                            reloadFridge('fruitList', u_fruitList);
+
+                            reloadFridge('miscList', u_miscList);
+
+                            //console.log("user: " + u_id);
+                            //console.log("protien List:" + u_proteinList[0]);
+
                             });
 
                         },
@@ -72,6 +95,51 @@
 
 }
  });
+
+    function reloadFridge(catagory, list){
+        for (var i = 0; i < list.length; i++) {
+            var item_name = list[i]; 
+
+            item_list.push(item_name);
+
+            var div = document.createElement('div');
+
+            div.setAttribute("class","fridgeItem");
+
+            id_counter++;
+
+            // x button to remove item
+            var close_x = document.createElement('span');
+            close_x.setAttribute("class", "closeX");
+            close_x.setAttribute("id","item" + id_counter);
+            close_x.setAttribute("title","click to remove");
+            close_x.setAttribute("onclick","removeItem(this)");
+            close_x.textContent = "x";
+
+            div.appendChild(close_x);
+
+            var name = document.createElement('span');
+            var name_id = "ItemId" + id_counter;
+
+            name.setAttribute("id", name_id);
+
+            name.setAttribute("class","theNameOfItem");
+
+
+            name.setAttribute("onclick", "renameItem(this)");
+
+
+            name.textContent = item_name;
+
+            div.appendChild(name);
+
+
+            document.getElementById(catagory).appendChild(div);
+            document.getElementById(catagory).style.padding = "10px";
+
+            console.log(item_list);
+        }
+    }
 
     function logout() {
         document.cookie = "login_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //sets cookie as past date to remove
